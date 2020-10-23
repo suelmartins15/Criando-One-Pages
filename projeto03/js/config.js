e0ad12c520b26f0f66cbcecfb94a5c45ef1506c3
Suel Martins de Oliveira <suelmartins15@gmail.com>
2020-10-23 13:14:30 -0300

    projeto3

---
+++ projeto03/js/config.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+    $('.scrollspy').scrollSpy({
+        scrollOffset: 90
+    });
+
+    $('.carousel.carousel-slider').carousel({
+        fullWidth: true,
+        indicators: true
+    });
+
+    $('.modal').modal();
+
+});
